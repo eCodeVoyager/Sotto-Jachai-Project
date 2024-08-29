b62aab9a86d0833b8d80fc4044773bae486fbedf
Salman Farshi <devwithfarshi@gmail.com>
2024-08-29 20:59:48 +0600

    ✅:update: sidebar UI with responsive

--- frontend/src/components/Layouts/DashBoardLayout.jsx
+++ frontend/src/components/Layouts/DashBoardLayout.jsx
@@ -4,9 +4,11 @@ import Sidebar from "../common/Sidebar";
 const DashBoardLayout = () => {
   return (
     <>
-      <main className="flex md:pe-4">
-        <Sidebar />
-        <div className="w-full *:ps-4 *:h-full *:overflow-y-auto py-4">
+      <main className="flex flex-col md:flex-row md:pe-4 md:py-5 min-h-screen ">
+        <aside className="md:px-5 flex items-center ">
+          <Sidebar />
+        </aside>
+        <div className="w-full">
           <Outlet />
         </div>
       </main>
--- frontend/src/components/common/Sidebar.jsx
+++ frontend/src/components/common/Sidebar.jsx
@@ -1,56 +1,82 @@
 import { navigation } from "@/router/routes.data";
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
+import profile_icon_sidebar from "@/assets/icons/profile_icon_sidebar.svg";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Logs } from "lucide-react";
 const userInfo = {
   name: "MD Salman Farshi",
   profile: "/profile.jpg",
   profession: "Software Engineer",
+  email: "devwithfarshi@gmail.com",
 };
 const Sidebar = () => {
   return (
     <>
-      {/* sidebar */}
-
-      <aside className="w-64 min-h-screen bg-gray-200 overflow-y-auto ">
-        <div className="py-3">
-          {/* profile info */}
-          <div className="flex flex-col items-center">
-            <figure className="size-36 border rounded-full border-gray-500 flex items-center justify-center bg-slate-500 text-slate-100">
-              {/* FIX: logo will here                     */}
-              {/* <h5 className="text-4xl font-extralight text-center">
-                Sotto <span>Jacai</span>
-              </h5> */}
-              <img
-                src="/logo.jpg"
-                alt=""
-                className="size-full object-cover rounded-full object-top"
-              />
-            </figure>
-            <div className="text-center mt-4">
-              <h2 className="font-medium text-gray-900 text-xl">
-                {userInfo.name}
-              </h2>
-              {userInfo.profession && (
-                <h4 className="font-medium text-base text-gray-600">
-                  {userInfo.profession}
-                </h4>
-              )}
-            </div>
-          </div>
-          {/* navigation */}
-          <ul className="mt-3 md:mt-6 flex flex-col gap-4 items-center">
+      <div className="w-full md:w-64 h-full bg-custom-50 overflow-y-auto md:rounded-2xl py-4 flex items-center justify-between md:items-stretch md:justify-normal md:flex-col px-4 md:px-0">
+        <figure className="shrink-0">
+          <img
+            className="mx-auto w-[150px] md:w-[200px]"
+            src="/images/logo.svg"
+            alt="sotto-jacai_logo"
+          />
+        </figure>
+        {/* navigation */}
+        <div className="flex-1 md:flex items-center w-full hidden">
+          <ul className="w-full px-2 space-y-3">
             {navigation.map((nav) => (
-              <li key={nav.id} className="w-full px-2">
+              <li key={nav.id} className="group">
                 <NavLink
                   to={nav.href}
-                  className="block rounded-lg duration-150 py-2 ps-10 font-medium text-base text-gray-900 hover:bg-gray-300 hover:text-gray-900"
+                  className="flex items-center gap-4   px-4 py-3 rounded-sm hover:bg-custom-100 duration-150"
                 >
-                  {nav.title}
+                  {<nav.icon className="group-hover:fill-white duration-150" />}
+                  <span className="font-openSans font-semibold text-text group-hover:text-[#EDEEEE] duration-150">
+                    {nav.title}
+                  </span>
                 </NavLink>
               </li>
             ))}
           </ul>
         </div>
-      </aside>
+        <div className="h-20 hidden md:flex justify-center gap-3">
+          <figure>
+            <img src={profile_icon_sidebar} alt="profile_icon_sidebar" />
+          </figure>
+          <div>
+            <h2 className="text-gray-700 font-semibold ">{userInfo.name}</h2>
+            <h4 className="text-muted-foreground text-sm">{userInfo.email}</h4>
+          </div>
+        </div>
+        <div className="md:hidden">
+          <DropdownMenu>
+            <DropdownMenuTrigger>
+              <Logs />
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="*:cursor-pointer shadow-md text-xl">
+              <div>
+                <DropdownMenuLabel>{userInfo.name}</DropdownMenuLabel>
+                <p className="text-xs font-medium">{userInfo.email}</p>
+              </div>
+              <DropdownMenuSeparator />
+              {navigation.map((nav) => (
+                <DropdownMenuItem key={nav.id} asChild>
+                  <Link to={nav.href} className="flex items-center gap-4">
+                    {<nav.icon />}
+                    <span>{nav.title}</span>
+                  </Link>
+                </DropdownMenuItem>
+              ))}
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
+      </div>
     </>
   );
 };

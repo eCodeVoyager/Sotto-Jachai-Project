a5c68b6310c037981414eebf00cb0639765770b1
Salman Farshi <devwithfarshi@gmail.com>
2024-08-30 19:36:51 +0600

    🆙:fix: Navbar design issue fixed

--- frontend/src/components/shared/Navbar/index.jsx
+++ frontend/src/components/shared/Navbar/index.jsx
@@ -25,42 +25,42 @@ const Navbar = () => {
   return (
     <nav className="py-4 border-b border-b-gray-800">
       <Container>
-        <div className="flex items-center">
-          <figure>
+        <div className="flex items-center flex-col sm:flex-row gap-y-3">
+          <figure className="shrink-0">
             <Link to={routes.verification}>
               <img
-                className="w-[100px] md:w-auto"
+                className="w-auto"
                 src="/images/logo.svg"
                 alt="sotto-jacai"
               />
             </Link>
           </figure>
-          <ul className="flex items-center ml-auto md:mx-auto gap-5 md:gap-7 font-medium text-gray-900 text-base">
-            {navData.map((item) => (
-              <li key={item.id}>
-                <Link to={item.href}>{item.title}</Link>
-              </li>
-            ))}
-          </ul>
-          {!Cookie.get("token") ? (
-            <Link
-              to={routes.register}
-              className={buttonVariants({
-                variant: "ghost",
-              })}
-            >
-              Register
-            </Link>
-          ) : (
-            <Link
-              to={routes.dashboard}
-              className={buttonVariants({
-                variant: "ghost",
-              })}
-            >
-              Dashboard
-            </Link>
-          )}
+          <div className="flex items-center justify-between flex-1">
+            <ul className="flex items-center ml-auto md:mx-auto gap-5 md:gap-7 font-medium text-gray-900 text-base">
+              {navData.map((item) => (
+                <li key={item.id}>
+                  <Link to={item.href}>{item.title}</Link>
+                </li>
+              ))}
+            </ul>
+            <div>
+              {!Cookie.get("token") ? (
+                <Link
+                  to={routes.register}
+                  className={"ml-5 font-medium text-gray-900 text-base"}
+                >
+                  Register
+                </Link>
+              ) : (
+                <Link
+                  to={routes.dashboard}
+                  className={"ml-5 font-medium text-gray-900 text-base"}
+                >
+                  Dashboard
+                </Link>
+              )}
+            </div>
+          </div>
         </div>
       </Container>
     </nav>
